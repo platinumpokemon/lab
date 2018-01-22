a533306c4588982ce004e6283be5d000a45fe0a8
platinumpokemon <montey2014@yahoo.com>
2018-01-22 16:52:10 -0600

    updt

---
+++ info.js
@@ -0,0 +1,20 @@
+// if (true) {
+//     var outcome = "if block";
+// } else if (true) {
+//     var outcome = "else if block";
+// } else {
+//     var outcome = "else block";
+// }
+
+var btn = document.createElement('button');
+btn.className = "box-button";
+btn.innerText = "squares";
+document.body.appendChild(btn);
+
+let box = document.createElement('div');
+box.className = "design";
+document.body.appendChild(box);
+
+
+
+
